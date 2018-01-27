caaac6c5c454c77476bdb7c1ef20fdc18316e8d2
Aidan Heller <asfheller@gmail.com>
2018-01-26 21:23:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Just For Fun
+
+A game I'm making just because I'm board, don't really know what it'll be yet 😂
---
+++ src/main/kotlin/com/jamdan2/justforfun/main.kt
@@ -0,0 +1,5 @@
+package com.jamdan2.justforfun
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
